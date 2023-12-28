1c8fbbcce345016c477930812105877ab43696f9
mystiskemorten <chrihh@proton.me>
2023-12-28 14:13:12 +0100

    Change lualine seperators

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.options.component_separators = { left = "⎢", right = "⎥" }
+      opts.options.section_separators = { left = "", right = "" }
+    end,
+  },
+}
