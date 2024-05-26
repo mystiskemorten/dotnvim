56b00850f21ae46e30ccdf91da138173058e6c94
mystiskemorten <chrihh@proton.me>
2024-05-26 14:38:24 +0200

    Try flash and nvim-notify again

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,12 +1,9 @@
 return {
 
-  { "flash.nvim", enabled = false },
   { "nvim-spectre", enabled = false },
   { "vim-illuminate", enabled = false },
-  { "nvim-notify", enabled = false },
   { "dressing.nvim", enabled = false },
   { "noice.nvim", enabled = false },
-  -- { "nui.nvim", enabled = false },
   { "dashboard-nvim", enabled = false },
   { "headlines.nvim", enabled = false },
 }
