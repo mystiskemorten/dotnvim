084009a42c6b2d38c57b8aee20be266c406de8cc
mystiskemorten <chrihh@proton.me>
2024-07-17 13:26:49 +0200

    Add headlines again

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,5 @@ return {
   { "dressing.nvim", enabled = false },
   { "noice.nvim", enabled = false },
   { "dashboard-nvim", enabled = false },
-  { "headlines.nvim", enabled = false },
   { "neo-tree.nvim", enabled = false },
 }
