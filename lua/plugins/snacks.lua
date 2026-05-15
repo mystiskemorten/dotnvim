4cebc158b68d020e486e99778d043cc874188d34
mystiskemorten <chrihh@proton.me>
2026-05-15 13:09:16 +0200

    Add horizontal preview in snacks picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -14,6 +14,41 @@ return {
       dashboard = {
         enabled = false,
       },
+      picker = {
+        layout = {
+          preset = "vertical_fullscreen",
+        },
+        layouts = {
+          vertical_fullscreen = {
+            layout = {
+              backdrop = false,
+              -- width = 0.5,
+              -- min_width = 80,
+              -- height = 0.9,
+              -- min_height = 28,
+              box = "vertical",
+              border = false,
+              title = "{title} {live} {flags}",
+              title_pos = "center",
+              {
+                win = "input",
+                height = 1,
+                border = "bottom",
+              },
+              {
+                win = "list",
+                border = "none",
+              },
+              {
+                win = "preview",
+                title = "{preview}",
+                border = "top",
+                height = 0.8,
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
